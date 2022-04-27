630b9c95ff84f73546eec5e1ec877c915c367943
HernandezEduin <86664284+HernandezEduin@users.noreply.github.com>
2022-04-28 02:48:35 +0300

    Update main.py

--- discriminators/main.py
+++ discriminators/main.py
@@ -10,6 +10,7 @@ import os
 
 from discriminators_all import discriminator_models as dm
 from authors import get_train_path, get_test_path
+from authors import authors as authors_dict
 from tools import open_authors_list, author_text_prepocess, shuffle, read_file
 
 def str2bool(string):
@@ -27,12 +28,12 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Author Text Classifier')
 
     'Model Details'
-    parser.add_argument('--model-name', type=str, default='svm', help='Model to use.') #120
+    parser.add_argument('--model-name', type=str, default='svm', help='Model to use. Can be ' + str(list(dm.keys()))) #120
     parser.add_argument('--train', type=str2bool, default='False', help='Whether to retrain model if it exists')
     parser.add_argument('--save', type=str2bool, default='True', help='Whether to save model.')
 
-    parser.add_argument('--show-train-metrics', type=str2bool, default='True', help='Calculate and Show Train metrics')
-    parser.add_argument('--show-val-metrics', type=str2bool, default='True', help='Calculate and Show Train metrics')
+    parser.add_argument('--show-train-metrics', type=str2bool, default='False', help='Calculate and Show Train metrics')
+    parser.add_argument('--show-val-metrics', type=str2bool, default='False', help='Calculate and Show Train metrics')
     parser.add_argument('--show-test-metrics', type=str2bool, default='True', help='Calculate and Show Train metrics')
 
     parser.add_argument('--test-filepath', type=str, default="../Dataset/William Shakespeare/shakespeare_valid.txt", help='Test Text to Evaluate')
@@ -43,6 +44,8 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
+    
+    assert args.model_name.lower() in list(dm.keys()), 'Error, ' + args.model_name + ' not in the list of models.'
     
     x_train_path, y_train = get_train_path()
     x_train = open_authors_list(x_train_path)
@@ -73,3 +76,17 @@ if __name__ == '__main__':
         predicted = model.predict(x_val)
         print('Validation Metric Results:')
         print(model.classification_report(y_val, predicted))
+    
+    if args.show_test_metrics and os.path.exists(args.test_filepath):
+        x_test, _ = read_file(args.test_filepath)
+        _,_, y_test = authors_dict[args.test_author.lower()]
+        x_test, y_test = [x_test], [y_test]
+        
+        x_test, y_test = author_text_prepocess(x_test, y_test)
+        predicted = model.predict(x_test)
+        
+        print('Test Metrics Results')
+        print('Accuracy: ', 100*sum(y_test == predicted)/len(y_test))
+        print('Correct: ', sum(y_test == predicted))
+        print('Incorrect: ', sum(y_test != predicted))
+        print('Sample size: ', len(y_test))
